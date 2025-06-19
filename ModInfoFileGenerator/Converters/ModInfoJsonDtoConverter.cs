49f1432a45e76864cc5254e498953ffb03e60468
ApacheTech Solutions <p_matthews@live.co.uk>
2025-06-19 19:37:12 +0100

    refactor: changes to dto converter

--- ModInfoFileGenerator/Converters/ModInfoJsonDtoConverter.cs
+++ ModInfoFileGenerator/Converters/ModInfoJsonDtoConverter.cs
@@ -1,6 +1,4 @@
-﻿using Vintagestory.API.Common;
-
-namespace ModInfoFileGenerator.Converters;
+﻿namespace ModInfoFileGenerator.Converters;
 
 /// <summary>
 ///     Converts <see cref="ModInfoAttribute"/> from a given assembly, to a <see cref="ModInfoJsonDto"/>, ready to be processed.
@@ -16,9 +14,27 @@ internal static class ModInfoJsonDtoConverter
     /// <exception cref="CustomAttributeFormatException">No ModInfoAttribute found in assembly.</exception>
     internal static ModInfoJsonDto PopulateJsonDto(this Assembly assembly, PackagerCommandLineArgs options)
     {
-        var modInfo = ExtractModFileInfo(assembly) 
-            ?? throw new CustomAttributeFormatException("No ModInfoAttribute found in assembly.");
-        return modInfo.ToDto(assembly, options);
+        var modInfoAttribute = ExtractModFileInfo(assembly) 
+            ?? throw new CustomAttributeFormatException("No ModInfoAttribute found in assembly."); var side = ParseAppSideOrThrow(modInfoAttribute.Side);
+        var version = ResolveVersion(modInfoAttribute, assembly, options);
+        var dependencies = assembly.FindAllModDependencies();
+        return new ModInfoJsonDto
+        {
+            Schema = options.SchemaUrl,
+            Type = "Code",
+            Name = modInfoAttribute.Name,
+            ModId = modInfoAttribute.ModID,
+            Side = modInfoAttribute.Side,
+            Description = modInfoAttribute.Description,
+            Version = version,
+            Authors = modInfoAttribute.Authors,
+            Contributors = modInfoAttribute.Contributors,
+            NetworkVersion = modInfoAttribute.NetworkVersion,
+            RequiredOnClient = modInfoAttribute.RequiredOnClient && side is not EnumAppSide.Server,
+            RequiredOnServer = modInfoAttribute.RequiredOnServer && side is not EnumAppSide.Client,
+            Website = modInfoAttribute.Website,
+            Dependencies = dependencies
+        };
     }
 
     /// <summary>
@@ -43,43 +59,23 @@ internal static class ModInfoJsonDtoConverter
     }
 
     /// <summary>
-    ///     Converts a vanilla  <see cref="ModInfoAttribute"/> to a <see cref="ModInfoJsonDto"/>, ready for serialisation.
+    ///     Parses the mod side string to EnumAppSide or throws if invalid.
     /// </summary>
-    /// <param name="modInfoAttribute">The mod information from the mod assembly.</param>
-    /// <param name="options">The parsed args passed in by the user.</param>
-    /// <param name="dependencies">A list of any specified mod dependencies, to add to the DTO.</param>
-    /// <returns></returns>
-    /// <exception cref="ArgumentException">Cannot parse '{modInfo.Side}', must be either 'Client', 'Server' or 'Universal'. Defaulting to 'Universal'.</exception>
-    private static ModInfoJsonDto ToDto(this ModInfoAttribute modInfoAttribute, Assembly assembly, PackagerCommandLineArgs options)
+    private static EnumAppSide ParseAppSideOrThrow(string sideString)
     {
-        if (!Enum.TryParse(modInfoAttribute.Side, true, out EnumAppSide side))
-            throw new ArgumentException(
-                $"Cannot parse '{modInfoAttribute.Side}', must be either 'Client', 'Server' or 'Universal'. Defaulting to 'Universal'.");
+        if (!Enum.TryParse(sideString, true, out EnumAppSide side))
+            throw new ArgumentException($"Cannot parse '{sideString}', must be either 'Client', 'Server' or 'Universal'. Defaulting to 'Universal'.");
+        return side;
+    }
 
-        var version = options.VersionType == "static"
+    /// <summary>
+    ///     Resolves the version string based on options.
+    /// </summary>
+    private static string ResolveVersion(ModInfoAttribute modInfoAttribute, Assembly assembly, PackagerCommandLineArgs options)
+    {
+        return options.VersionType == "static"
             ? modInfoAttribute.Version
             : FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion!;
-
-        var dependencies = assembly.FindAllModDependencies();
-
-        var dto = new ModInfoJsonDto
-        {
-            Schema = options.SchemaUrl,
-            Type = "Code",
-            Name = modInfoAttribute.Name,
-            ModId = modInfoAttribute.ModID,
-            Side = modInfoAttribute.Side,
-            Description = modInfoAttribute.Description,
-            Version = version,
-            Authors = modInfoAttribute.Authors,
-            Contributors = modInfoAttribute.Contributors,
-            NetworkVersion = modInfoAttribute.NetworkVersion,
-            RequiredOnClient = modInfoAttribute.RequiredOnClient && side is not EnumAppSide.Server,
-            RequiredOnServer = modInfoAttribute.RequiredOnServer && side is not EnumAppSide.Client,
-            Website = modInfoAttribute.Website,
-            Dependencies = dependencies
-        };
-        return dto;
     }
 
     /// <summary>
